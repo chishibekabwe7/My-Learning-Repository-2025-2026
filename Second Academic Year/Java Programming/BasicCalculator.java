96564e6c6bad8f48d941e9eb75a35b1f2dbcfa0c
chishibekabwe7 <chishibekabwe7gmail.com>
2025-10-08 03:08:31 +0200

    Day 2 of Java programming

---
+++ Second Academic Year/Java Programming/BasicCalculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class BasicCalculator {
+public static void main (String [] args)    {
+     int firstNumber  = 10;
+     int secondNumber = 24;
+    
+     int sum = firstNumber + secondNumber;
+     int quotient = firstNumber / secondNumber;
+     int difference = firstNumber - secondNumber;
+     int product = firstNumber * secondNumber;
+    
+     System.out.println("            Basic Calculator Program            ");
+     System.out.println("The firstnumber is: " + firstNumber);
+     System.out.println("The secondnumber is: " + secondNumber);
+     System.out.println("The sum is: " + sum);
+     System.out.println("The quotient is: " + quotient);
+     System.out.println("The difference is: " + difference);
+     System.out.println("The product is: " + product);
+
+
+}
+}
---
+++ Second Academic Year/Java Programming/W3schools.java
@@ -0,0 +1,33 @@
+//W3Schools java basics tutorial
+public class W3schools {
+    public static void main(String[] args){
+        //Hello World
+        System.out.println("Hello World!");
+
+        //This is java comment
+
+        //Variables
+        String firstName = "Wange ";
+        String lastName = "Shula";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+        
+        //Declaring multiple variables
+        int x,y,z;
+        x = y = z = 50;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
+        //Constant Variable
+        final int num = 10;
+        // This >>> num = 11; will be an error because of the final keyword
+        System.out.println(num);
+
+        
+
+
+
+
+    }
+}
