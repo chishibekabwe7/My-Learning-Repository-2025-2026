6f0a2a85fafd31da69f02565a105c9c06231a8e2
Chishibe <chishibekabwe7@gmail.com>
2026-04-12 05:20:09 +0200

    Module6: add earthquake sorting and airport route-click interaction with hover popups

--- Second Academic Year/Coursera/University of California San Diego/UCSDUnfoldingMaps/src/module6/AirportMap.java
+++ Second Academic Year/Coursera/University of California San Diego/UCSDUnfoldingMaps/src/module6/AirportMap.java
@@ -1,19 +1,17 @@
 package module6;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 import de.fhpotsdam.unfolding.UnfoldingMap;
 import de.fhpotsdam.unfolding.data.PointFeature;
 import de.fhpotsdam.unfolding.data.ShapeFeature;
+import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.marker.Marker;
 import de.fhpotsdam.unfolding.marker.SimpleLinesMarker;
-import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 import de.fhpotsdam.unfolding.utils.MapUtils;
-import de.fhpotsdam.unfolding.geo.Location;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import parsing.ParseFeed;
-import processing.core.PApplet;
+
 
 /** An applet that shows airports (and routes)
  * on a world map.  
@@ -25,7 +23,10 @@ public class AirportMap extends PApplet {
 	
 	UnfoldingMap map;
 	private List<Marker> airportList;
-	List<Marker> routeList;
+	private List<Marker> routeList;
+	private HashMap<String, List<Marker>> routesBySource;
+	private CommonMarker lastSelected;
+	private CommonMarker lastClicked;
 	
 	public void setup() {
 		// setting up PAppler
@@ -41,6 +42,7 @@ public class AirportMap extends PApplet {
 		// list for markers, hashmap for quicker access when matching with routes
 		airportList = new ArrayList<Marker>();
 		HashMap<Integer, Location> airports = new HashMap<Integer, Location>();
+		routesBySource = new HashMap<String, List<Marker>>();
 		
 		// create markers from features
 		for(PointFeature feature : features) {
@@ -68,21 +70,22 @@ public class AirportMap extends PApplet {
 			if(airports.containsKey(source) && airports.containsKey(dest)) {
 				route.addLocation(airports.get(source));
 				route.addLocation(airports.get(dest));
+
+				SimpleLinesMarker sl = new SimpleLinesMarker(route.getLocations(), route.getProperties());
+				sl.setColor(color(220, 220, 220));
+				sl.setStrokeWeight(1);
+				sl.setHidden(true);
+				routeList.add(sl);
+
+				String sourceKey = route.getProperty("source").toString();
+				if (!routesBySource.containsKey(sourceKey)) {
+					routesBySource.put(sourceKey, new ArrayList<Marker>());
+				}
+				routesBySource.get(sourceKey).add(sl);
 			}
-			
-			SimpleLinesMarker sl = new SimpleLinesMarker(route.getLocations(), route.getProperties());
-		
-			System.out.println(sl.getProperties());
-			
-			//UNCOMMENT IF YOU WANT TO SEE ALL ROUTES
-			//routeList.add(sl);
 		}
 		
-		
-		
-		//UNCOMMENT IF YOU WANT TO SEE ALL ROUTES
-		//map.addMarkers(routeList);
-		
+		map.addMarkers(routeList);
 		map.addMarkers(airportList);
 		
 	}
@@ -91,6 +94,61 @@ public class AirportMap extends PApplet {
 		background(0);
 		map.draw();
 		
+	}
+
+	@Override
+	public void mouseMoved()
+	{
+		if (lastSelected != null) {
+			lastSelected.setSelected(false);
+			lastSelected = null;
+		}
+
+		for (Marker marker : airportList) {
+			CommonMarker airport = (CommonMarker) marker;
+			if (airport.isInside(map, mouseX, mouseY)) {
+				lastSelected = airport;
+				airport.setSelected(true);
+				return;
+			}
+		}
+	}
+
+	@Override
+	public void mouseClicked()
+	{
+		if (!checkAirportsForClick()) {
+			hideAllRoutes();
+			lastClicked = null;
+		}
+	}
+
+	private boolean checkAirportsForClick()
+	{
+		for (Marker marker : airportList) {
+			if (!marker.isHidden() && marker.isInside(map, mouseX, mouseY)) {
+				lastClicked = (CommonMarker) marker;
+				hideAllRoutes();
+
+				List<Marker> routesFromAirport = routesBySource.get(marker.getId());
+				if (routesFromAirport != null) {
+					for (Marker route : routesFromAirport) {
+						route.setHidden(false);
+					}
+				}
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private void hideAllRoutes()
+	{
+		for (Marker route : routeList) {
+			route.setHidden(true);
+		}
 	}
 	
 
--- Second Academic Year/Coursera/University of California San Diego/UCSDUnfoldingMaps/src/module6/AirportMarker.java
+++ Second Academic Year/Coursera/University of California San Diego/UCSDUnfoldingMaps/src/module6/AirportMarker.java
@@ -1,42 +1,59 @@
 package module6;
 
-import java.util.List;
-
 import de.fhpotsdam.unfolding.data.Feature;
 import de.fhpotsdam.unfolding.data.PointFeature;
-import de.fhpotsdam.unfolding.marker.SimpleLinesMarker;
+import processing.core.PConstants;
 import processing.core.PGraphics;
 
-/** 
+/**
  * A class to represent AirportMarkers on a world map.
- *   
+ *
  * @author Adam Setters and the UC San Diego Intermediate Software Development
  * MOOC team
  *
  */
 public class AirportMarker extends CommonMarker {
-	public static List<SimpleLinesMarker> routes;
-	
+
 	public AirportMarker(Feature city) {
-		super(((PointFeature)city).getLocation(), city.getProperties());
-	
+		super(((PointFeature) city).getLocation(), city.getProperties());
+		setId(city.getId());
 	}
-	
+
 	@Override
 	public void drawMarker(PGraphics pg, float x, float y) {
+		pg.pushStyle();
 		pg.fill(11);
 		pg.ellipse(x, y, 5, 5);
-		
-		
+		pg.popStyle();
 	}
 
 	@Override
 	public void showTitle(PGraphics pg, float x, float y) {
-		 // show rectangle with title
-		
-		// show routes
-		
-		
+		String name = getStringProperty("name");
+		String city = getStringProperty("city");
+
+		if (name == null) {
+			name = "Unknown Airport";
+		}
+		if (city == null) {
+			city = "Unknown City";
+		}
+
+		name = name.replace("\"", "");
+		city = city.replace("\"", "");
+
+		pg.pushStyle();
+		pg.rectMode(PConstants.CORNER);
+		pg.fill(255, 255, 255);
+		pg.stroke(0);
+		float boxWidth = Math.max(pg.textWidth(name), pg.textWidth(city)) + 8;
+		pg.rect(x, y + 10, boxWidth, 34, 5);
+
+		pg.fill(0);
+		pg.textAlign(PConstants.LEFT, PConstants.TOP);
+		pg.text(name, x + 4, y + 13);
+		pg.text(city, x + 4, y + 27);
+		pg.popStyle();
 	}
-	
+
 }
